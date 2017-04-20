530ca2d168698f688234c34a6bc1cc3fe26cff14
Mateusz Popielarz <pixellos@users.noreply.github.com>
2017-04-20 21:12:38 +0200

    Virgardium Trollosa

--- HideTrols.js
+++ HideTrols.js
@@ -8,7 +8,10 @@ var trolls = JSON.parse(xmlhttp.responseText);
 
 trolls.forEach(function(trol)
 {
-$(".col-user > h5 > a").filter(function() { return $(this).text() == trol;}).each(function() { $(this).parent().parent().parent().parent().remove();  })
-$(".media > .media-body > p > a").filter(function() { return $(this).text() == trol;}).each(function() { $(this).parent().parent().parent().remove(); })
-$(".media-body > .media-heading > a").filter(function() { return $(this).text() ==  trol;}).each( function() {$(this).parent().parent().parent().remove();} )
+	$(".col-user > h5 > a").filter(function() { return $(this).text() == trol;}).each(function() { $(this).parent().parent().parent().parent().remove();  })
+	$(".media > .media-body > p > a").filter(function() { return $(this).text() == trol;}).each(function() { $(this).parent().parent().parent().remove(); })
+	//Mikroblog
+	$(".media-body > .media-heading > a").filter(function() { return $(this).text() ==  trol;}).each( function() {$(this).parent().parent().parent().remove();} )
+	//Komentarz do mikrobloga
+	$(".comment-body > h6 > a").filter(function() { return $(this).text() == trol;}).each( function() {$(this).parent().parent().parent().parent().remove();} )
 });
