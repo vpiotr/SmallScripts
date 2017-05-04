60a0dcc23f434bd246378ef5510ec70448bda656
Mateusz Popielarz <pixellos@users.noreply.github.com>
2017-05-04 20:43:00 +0200

    It's arms race!

--- HideTrols.js
+++ HideTrols.js
@@ -5,13 +5,44 @@ xmlhttp.open(
 	false);
 xmlhttp.send();
 var trolls = JSON.parse(xmlhttp.responseText);
+var regexpSwitches = "i";
 
 trolls.forEach(function(trol)
 {
-	$(".col-user > h5 > a").filter(function() { return $(this).text() == trol;}).each(function() { $(this).parent().parent().parent().parent().remove();  })
-	$(".media > .media-body > p > a").filter(function() { return $(this).text() == trol;}).each(function() { $(this).parent().parent().parent().remove(); })
+	var trollReg = new RegExp(trol, regexpSwitches);
+	//Posty
+	$(".col-user > h5")
+		.filter(function() 
+		{ 
+			return $(this).text().match(trollReg);
+		}).each(function() 
+		{
+			$(this).parent().parent().parent().parent().remove();  
+		});
+	//Komentarz
+	$(".media > .media-body > p > a")
+		.filter(function() 
+		{
+			return $(this).text().match(trollReg);
+		}).each(function() {
+			$(this).parent().parent().parent().remove(); 
+		});
 	//Mikroblog
-	$(".media-body > .media-heading > a").filter(function() { return $(this).text() ==  trol;}).each( function() {$(this).parent().parent().parent().remove();} )
+	$(".media-body > .media-heading > a")
+		.filter(function() 
+		{ 
+			return $(this).text().match(trollReg);
+		}).each( function() 
+		{
+			$(this).parent().parent().parent().remove();
+		});
 	//Komentarz do mikrobloga
-	$(".comment-body > h6 > a").filter(function() { return $(this).text() == trol;}).each( function() {$(this).parent().parent().parent().parent().remove();} )
+	$(".comment-body > h6 > a")
+		.filter(function() 
+		{ 
+			return $(this).text().match(trollReg);
+		}).each( function() 
+		{
+			$(this).parent().parent().parent().parent().remove();
+		});
 });
